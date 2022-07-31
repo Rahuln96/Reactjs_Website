e9ae58dc0cae89e5485eb4e84862d0451e749e7e
Rahul <rahulk6491@gmail.com>
2022-07-30 22:02:02 -0700

    abc

---
+++ documentmanage/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import img from './Component/img/1.jpg'
+import './App.css';
+import './style.css';
+import Header from './Component/Header/Header'
+import Footer from './Component/Header/Footer'
+import Home from './Component/MainComponent/Home'
+import About from './Component/MainComponent/About'
+import Contact from './Component/MainComponent/Contact'
+import Service from './Component/MainComponent/Service'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+       <BrowserRouter>
+       <Header />
+          <Routes>
+               <Route path='/' element = {<Home />} />
+               <Route path='/About' element = {<About />} />
+               <Route path='/Contact' element = {<Contact />} />
+               <Route path='/Service' element = {<Service />} />
+            
+
+          </Routes>
+       
+       <Footer />
+       </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ documentmanage/src/Component/MainComponent/Home.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import img2 from './../img/2.jpg'
+import img3 from './../img/3.jpg'
+
+export default function Home() {
+  return (
+    <React.Fragment>
+    <div>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src= './../image/1.jpg' class="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={img2} className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={img3} className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+    </React.Fragment>
+  )
+}
